ecf3a80108e2c74d7b20a08097408a1505cc4b0a
Nathan Vu <nathanvu@Nathans-MacBook-Pro.local>
2020-09-25 23:17:36 -0700

    Added functional time blocks and description saving

--- script.js
+++ script.js
@@ -3,6 +3,66 @@
 
 let container = document.getElementsByClassName('container');
 let currentDay = document.getElementById('currentDay');
+//console.log(currentHour);
+
+const defaultData = {
+  data: [],
+};
+
+for (let i = 0; i < 9; i++) {
+  defaultData.data.push('');
+}
+
+const updateData = (i) => {
+  const textArea = document.getElementById(i);
+  const currentData = JSON.parse(window.localStorage.getItem('alldata'));
+  currentData.data[i] = textArea.value;
+  window.localStorage.setItem('alldata', JSON.stringify(currentData));
+};
+
+const init = () => {
+  const main = document.getElementById('main');
+  const storedData = JSON.parse(window.localStorage.getItem('alldata'));
+  let dataToUse = storedData || defaultData;
+  
+  for (let i = 0; i < 20; i++) {
+    let hourNum = 9 + i; // 9 or 10 or 11 ...
+    let hourStr = hourNum.toString();
+    if (hourStr.length === 1) { // if single digit
+      hourStr = '0' + hourStr; // add 0 to the front
+    }
+    const hour = moment('2013-02-08T' + hourStr).format('LT')
+    const currentHour = moment().hour();
+
+    let colorClass = '';
+    if(hourNum < currentHour) {
+      colorClass = 'past';
+    } else if (hourNum == currentHour) {
+      colorClass = 'present';
+    } else {
+      colorClass = 'future';
+    }
+
+    const timeBlock = document.createElement('div');
+    timeBlock.innerHTML = `
+      <div class="container time-block ${colorClass}">
+        <div class="row">
+          <div class="col-2 hour text-right"><br>${hour}</div>
+          <textarea
+            class="col-9 w-100 description "
+            id="${i}"
+          >${dataToUse.data[i]}</textarea>
+          <div class="col-1 saveBtn" onClick="updateData(${i})"><i class="far fa-save"></i></div>
+        </div>
+      </div>
+    `;
+    // textArea.addEventListener('onchange', updateData);
+    
+    main.appendChild(timeBlock);
+  }
+  window.localStorage.setItem('alldata', JSON.stringify(dataToUse));
+};
+
 
 // Adds today's date to the top of the calendar
 function addDate() {
@@ -29,14 +89,8 @@ function addDate() {
   }
 }
 
-// Adds timeblocks to calendar
-function addBlock() {
-  let timeblock = document.createElement('div');
-  //container.push(timeblock);
-}
-
 // Changes timeblock color based on if it's in past, present, or future
-function changeBlock() {
+const changeBlock = () => {
 
 }
 
@@ -46,4 +100,4 @@ function saveBlock() {
 }
 
 addDate();
-addBlock();
+init();
